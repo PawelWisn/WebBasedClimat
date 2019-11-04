a871f1499202f31581e0706edb0cd8cd9f918482
julialisz <julia.lisznianska@gmail.com>
2019-11-04 12:19:50 +0100

    javascript functions

--- MyScript.js
+++ MyScript.js
@@ -1,10 +1,13 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed!";
+function changeParagraph()
+{
+    document.getElementById("par_change").innerHTML = "Paragraph changed!";
 }
 
-var alternateCase = function (s) {
+var alternateCase = function (s)
+{
     var chars = s.toLowerCase().split("");
-    for (var i = 0; i < chars.length; i ++) {
+    for (var i = 0; i < chars.length; i ++)
+    {
         chars[i] = chars[i].toUpperCase();
     }
     return chars.join("");
@@ -13,7 +16,8 @@ var alternateCase = function (s) {
 var txt = "hello world";
 console.log(alternateCase(txt));
 
-function myFunction() {
+function myFunction()
+{
     var x =
         document.getElementById("myText").value;
     var chars = x.toUpperCase();
@@ -21,4 +25,26 @@ function myFunction() {
     document.getElementById(
         "change_name").innerHTML = chars;
 
+}
+
+function iteratingCauses()
+{
+    var causes = ['Wasting water', 'Using coal', 'Driving cars'];
+    var x;
+
+    for (x of causes) {
+        document.write(x + "<br >");
+    }
+}
+
+function iter_whileCauses()
+{
+    var causess = ['Wasting water', 'Using coal', 'Driving cars'];
+    var i = 0;
+    var text = "";
+    while (causess[i]) {
+        text += causess[i] + "<br>";
+        i++;
+    }
+    document.getElementById("iter_causes").innerHTML = text;
 }
