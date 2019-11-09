07fce77449b42df57c61d85856bfb4839dc6734f
pawel <wispawelwis38@gmail.com>
2019-11-09 15:39:40 +0100

    removed some awful css xD upgraded js moved all js to MyScript.js

--- MyScript.js
+++ MyScript.js
@@ -1,50 +1,86 @@
-function changeParagraph()
-{
-    document.getElementById("par_change").innerHTML = "Paragraph changed!";
+// homepage.html
+Date.prototype.getSecondsStr = function () {
+    let seconds = new Date().getSeconds();
+    return seconds < 10 ? '0' + seconds : seconds;
+};
+
+Date.prototype.getHoursStr = function () {
+    let hours = new Date().getHours();
+    return hours < 10 ? '0' + hours : hours;
+};
+
+Date.prototype.getMinutesStr = function () {
+    let minutes = new Date().getMinutes();
+    return minutes < 10 ? '0' + minutes : minutes;
+};
+
+const d = new Date();
+const clock = document.getElementById("clock");
+
+function updateClock() {
+    clock.innerHTML = d.getHoursStr() + ':' + d.getMinutesStr() + ':' + d.getSecondsStr();
+    d.setSeconds(d.getSeconds() + 1);
 }
 
-let alternateCase = function (s)
-{
+// causes.html
+function changeParagraph() {
+    document.getElementById("par_change").innerHTML = "A greenhouse gas is a gas that absorbs and emits radiant energy within the thermal infrared range.";
+}
+
+function fillIterativeCauses() {
+    console.log("fillIterativeCauses");
+    let parList = document.getElementsByClassName('iteratingCauses');
+    iteratingCausesFor(parList[0]);
+    iteratingCausesWhile(parList[1]);
+}
+
+function iteratingCausesFor(par) {
+    console.log("iteratingCausesFor");
+    let causes = ['Wasting water', 'Using coal', 'Internal combustion engines'];
+    let newHTML = par.innerHTML + "\n<ul>";
+    for (let elem of causes) {
+        newHTML += "\n<li>" + elem + "</li>";
+    }
+    newHTML += "\n</ul>";
+    par.innerHTML = newHTML;
+}
+
+function iteratingCausesWhile(par) {
+    console.log("iteratingCausesWhile");
+    let causes = ['Wasting water', 'Using coal', 'Internal combustion engines'];
+    let i = 0;
+    let newHTML = par.innerHTML + "\n<ul>";
+    while (i < causes.length) {
+        newHTML += "\n<li>" + causes[i] + "</li>";
+        i++;
+    }
+
+    newHTML += "\n</ul>";
+    par.innerHTML = newHTML;
+}
+
+// console
+let alternateCase = function (s) {
     let chars = s.toLowerCase().split("");
-    for (let i = 0; i < chars.length; i ++)
-    {
+    for (let i = 0; i < chars.length; i++) {
         chars[i] = chars[i].toUpperCase();
     }
     return chars.join("");
 };
 
-let txt = "hello world";
+let txt = "this text has been changed to upper case";
 console.log(alternateCase(txt));
 
-function myFunction()
-{
-    let x =
-        document.getElementById("myText").value;
-    let chars = x.toUpperCase();
 
-    document.getElementById(
-        "change_name").innerHTML = chars;
-
-}
-
-function iteratingCauses()
-{
-    let causes = ['Wasting water', 'Using coal', 'Driving cars'];
-    let x;
-
-    for (x of causes) {
-        document.write(x + "<br >");
+// feedback.html
+function validateName() {
+    let name =
+        document.getElementById("feedbackName").value;
+    if (name[0] !== name[0].toUpperCase() || /\d/.test(name)) {
+        alert("Name should start with a capital letter and shouldn't contain any digits")
+    } else {
+        alert("Name is OK")
     }
+
 }
 
-function iter_whileCauses()
-{
-    let causes = ['Wasting water', 'Using coal', 'Driving cars'];
-    let i = 0;
-    let text = "";
-    while (causess[i]) {
-        text += causess[i] + "<br>";
-        i++;
-    }
-    document.getElementById("iter_causes").innerHTML = text;
-}
