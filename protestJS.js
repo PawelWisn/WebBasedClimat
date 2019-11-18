2958e0f5962802eda819e60edd05a518afaf44b5
pawel <wispawelwis38@gmail.com>
2019-11-18 14:16:34 +0100

    minheight of main changes on reload and resize

--- protestJS.js
+++ protestJS.js
@@ -1,2 +1,19 @@
+// $('main').append($('<p>hi there</p>'));
 
-$('main').append($('<p>hi there</p>'));
+//+ parseInt($nav.css('marginTop')) + parseInt($nav.css('marginBottom'));
+function setMainMinHeight() {
+    let $logo = $('header a img').outerHeight(true);
+    let $nav = $('nav').outerHeight(true);
+    let $foot = $('footer').outerHeight(true);
+    let $main = $('main').first();
+
+    let newMinHeight = $(window).height()
+        - $logo
+        - $nav
+        - $foot
+        + 'px';
+
+    $($main).css('minHeight', newMinHeight);
+}
+$(setMainMinHeight); //run when document loaded
+$(window).on('resize', setMainMinHeight);
